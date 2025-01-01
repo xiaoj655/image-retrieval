1bcdc78356aa82eb595d0cf71b6760d67f1bcfa9
brook <xiaoj655@gmail.com>
2025-01-02 00:45:58 +0800

    update frontend

--- app/frontend/src/views/Home.vue
+++ app/frontend/src/views/Home.vue
@@ -94,13 +94,13 @@ watch(t, () => {
 <div class="flex flex-col gap-4">
     <div class="text-2xl font-bold flex items-center gap-2">
         基于Resnet预训练模型的电影图像检索
-        <a href="">
+        <a href="https://github.com/xiaoj655/image-retrieval" target="_self">
         <svg viewBox="0 0 24 24" aria-hidden="true" class="size-6 fill-slate-900"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"></path></svg>
         </a>
     </div>
     <div class="border-b-2 border-black">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, tenetur! Cum impedit quisquam dicta aperiam iusto deleniti omnis veritatis ab quas repellat! Enim aut ea, maiores molestias necessitatibus ab vitae?
-        Expedita in enim ducimus ea quas corrupti. Earum est dolores sint reiciendis nemo consequatur laboriosam neque iste? Deserunt blanditiis exercitationem accusamus ducimus commodi corrupti ipsum? In excepturi minus eos exercitationem?
+        在Resnet101预训练模型微调, 完成图像检索任务.
+        数据集包括<a href="https://image-retrieval.bitdancing.com/movie_list" target="_blank">223部电影</a>片段, 通过计算平均哈希的方式, 筛选出30万张图片作为数据集
     </div>
     <div class="text-2xl font-bold">
         上传图像
